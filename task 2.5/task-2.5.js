691aa5cf92bed569cd05736911e15ddfa53bcd17
Vlad-dwww <accountforart@yandex.ru>
2024-05-09 10:30:54 +0500

    2.5 task solution

--- task 2.5/task-2.5.js
+++ task 2.5/task-2.5.js
@@ -1,19 +1,46 @@
-const screenPrice = 1000;
+let screenPrice;
 let percentage = 10;
-let titleProject = prompt('Название проекта?')
-let screensValue = prompt('шаблонные, с уникальным дизайном, с анимациями')
-let responsive = confirm('Нужен ли респонсивный сайт?')
-// let service1 = prompt('Какой сервис нужен?')
-// let servicePrice1 = +prompt('Сколько это будет стоить?')
-// let service2 = prompt('Какой еще сервис тебе нужен?')
-// let servicePrice2 = +prompt('Сколько будет стоить этот второй сервис?')
 let service1
-let service2
+let service2 
 
-console.log(screensValue);
-console.log(responsive); 
-console.log(service1);
-console.log(service2);
+const checkIsNumber = function(number) {
+    return !isNaN(parseFloat(number)) && isFinite(number)
+}
+
+const asking = function() {
+    titleProject = prompt('Название проекта?')
+    screensValue = prompt('шаблонные, с уникальным дизайном, с анимациями')
+    responsive = confirm('Нужен ли респонсивный сайт?')
+
+    screenPrice = prompt('Сколько это будет стоить?', 10000)
+
+    while (!checkIsNumber(screenPrice) || screenPrice === null || screenPrice.trim() === '') {
+        screenPrice = prompt('Сколько это будет стоить?', 10000)
+    }
+    screenPrice = Number(screenPrice)
+}
+
+const getAllServicePrices = function() {
+    let sum = 0
+
+    for (let i = 0; i < 2; i++) {
+        if (i === 0) {
+            service1 = prompt('Какой дополнительный тип услуги нужен', 'Услуга 1')
+        } else if (i === 1) {
+            service2 = prompt('Какой дополнительный тип услуги нужен', 'Услуга 2')
+        }
+
+        let textFromPrompt = '';
+
+        while (!checkIsNumber(textFromPrompt) || textFromPrompt === null || textFromPrompt.trim() === '') {
+            textFromPrompt = prompt('Сколько это будет стоить?')
+        }
+
+        sum = sum + Number(textFromPrompt)
+    }
+
+return sum
+}
 
 function getTitle(titleProject) {
     return titleProject.charAt(0).toUpperCase() + titleProject.slice(1);
@@ -22,19 +49,12 @@ function getTitle(titleProject) {
 let titleUpperCaseFirst = getTitle(titleProject);
 console.log(titleUpperCaseFirst); 
 
-// const getAllServicePrices = function(servicePrice1, servicePrice2) {
-//     return servicePrice1 + servicePrice2;
-// }
+function getFullPrice(allServicePrices, screenPrice) {
+    return allServicePrices + screenPrice;
+}
 
-// let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
-// console.log(allServicePrices);
-
-// function getFullPrice(allServicePrices, screenPrice) {
-//     return allServicePrices + screenPrice;
-// }
-
-// let fullPrice = getFullPrice(allServicePrices, screenPrice);
-// console.log(fullPrice); 
+let fullPrice = getFullPrice(allServicePrices, screenPrice);
+console.log(fullPrice); 
 
 function getServicePercentPrices(fullPrice, percentage) {
     return fullPrice - fullPrice/100*percentage;
@@ -68,35 +88,7 @@ function getRollbackMessage(fullPrice) {
 let discountAmount = getRollbackMessage(fullPrice, servicePercentPrice);
 console.log(discountAmount);
 
+let allServicePrices = getAllServicePrices();
+console.log(allServicePrices);
 
-
-
-const checkIsNumber = function(number) {
-    return !isNaN(parseFloat(number)) && isFinite(number)
-}
-
-const getAllServicePrices = function() {
-    let sum = 0
-
-    for (let i = 0; i < 2; i++) {
-        if (i === 0) {
-            service1 = prompt('Какой дополнительный тип услуги нужен', 'Услуга 1')
-        } else if (i === 1) {
-            service2 = prompt('Какой дополнительный тип услуги нужен', 'Услуга 2')
-        }
-
-        let textFromPrompt = '';
-
-        while (!checkIsNumber(textFromPrompt) || textFromPrompt === null || textFromPrompt.trim() === '') {
-            textFromPrompt = prompt('Сколько это будет стоить?')
-        }
-
-        sum = sum + Number(textFromPrompt)
-    }
-
-return sum
-}
-
-getAllServicePrices()
-// console.log(getAllServicePrices);
-console.log(getAllServicePrices());
+asking();
