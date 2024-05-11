15558253930f94a9acb691463718d6a48c549468
Vlad-dwww <accountforart@yandex.ru>
2024-05-11 13:52:08 +0500

    lesson 2.5 solution

--- task 2.6/task-2.6.js
+++ task 2.6/task-2.6.js
@@ -16,14 +16,14 @@ const gameBotFunction = function () {
         let guess = prompt("Угадайте число от 1 до 100. У вас есть " + attempts + " попытки");
 
         if (guess === null) {
-            console.log("Вы завершили игру");
+            alert("Вы завершили игру");
             return;
         }
 
         while (!checkIsNumber(guess) || guess === "") {
             guess = prompt("Введите корректное число").trim();
             if (guess === null) {
-                console.log("Вы завершили игру");
+                alert("Вы завершили игру");
                 return;
             }
         }
@@ -31,25 +31,25 @@ const gameBotFunction = function () {
         guess = parseInt(guess);
         
         if (guess === mysteryNumber) {
-            console.log("Поздравляю! Вы угалали число");
-            let playAgain = confirm("Хотите сыграть еще раз?");
-            if (playAgain) {
-                attempts = maxAttempts;
-                mysteryNumber = randomGenerate(1, 100);
-            } else {
-                console.log("Вы завершили игру");
-                return;
-            }
+            alert("Поздравляю! Вы угалали число");
         } else if (guess < mysteryNumber) {
-            console.log("Загаданное число больше. Попробуйте еще");
+            alert("Загаданное число больше. Попробуйте еще");
         } else {
-            console.log("Загаданное число меньше. Попробуйте еще");
+            alert("Загаданное число меньше. Попробуйте еще");
         }
         
         attempts--;
     }
     
-    console.log("Вы не угадали. Загаданное число:", mysteryNumber);
+    alert(`Вы не угадали. Загаданное число: ${mysteryNumber}`);
+    let playAgain = confirm("Хотите сыграть еще раз?");
+    if (playAgain) {
+        attempts = maxAttempts;
+        gameBotFunction();
+    } else {
+        alert("Вы завершили игру");
+        return;
+     }
 }
 
 gameBotFunction();
